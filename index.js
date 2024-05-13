cc90d332a32de0b6406e0388341feca5aaecf946
juwel-hub <jr3764920@gmail.com>
2024-05-14 01:16:13 +0600

    emplement jwt

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 //Must remove "/" from your production URL
@@ -16,8 +19,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zbpbuag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +33,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// middleware
+const logger = (req, res, next) => {
+  console.log("log:info", req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("token from middleWare", token);
+  if (!token) {
+    return res.status(401).send({ message: "unAuthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -41,13 +67,44 @@ async function run() {
       .db("deliciousFoodDB")
       .collection("addFoods");
 
+    // auth related api
+
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logout", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
     app.get("/featureFood", async (req, res) => {
       const cursor = featuredCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get("/manageFood/:email", async (req, res) => {
+    app.get("/manageFood/:email", logger, verifyToken, async (req, res) => {
+      console.log("cook cook", req.user);
+      if (req.user.email !== req.params.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      let query = {};
+      if (req.params?.email) {
+        query = { email: req.params.email };
+      }
       const result = await addFoodCollection
         .find({ email: req.params.email })
         .toArray();
@@ -58,6 +115,13 @@ async function run() {
     app.get("/addFoods", async (req, res) => {
       const cursor = addFoodCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/deleteData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addFoodCollection.deleteOne(query);
       res.send(result);
     });
 
